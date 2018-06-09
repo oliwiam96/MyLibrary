6e9a52bbffb0864b13e5c681d0f8e1a08a507e05
Oliwia <oliwia.m.96@wp.pl>
2018-06-09 22:11:36 +0200

    Send invitation email with token

--- MyLibrary/Controllers/FriendshipController.cs
+++ MyLibrary/Controllers/FriendshipController.cs
@@ -10,20 +10,15 @@ using MyLibrary.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+
 
 namespace MyLibrary.Controllers
 {
     [Authorize]
     public class FriendshipController : Controller
     {
-        private ApplicationDbContext db;
-        private UserManager<ApplicationUser> UserManager;
-
-        public FriendshipController()
-        {
-            db = new ApplicationDbContext();
-            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
-        }
 
         // GET: Friendship/Invite
         public ActionResult Invite()
@@ -33,19 +28,21 @@ namespace MyLibrary.Controllers
 
         // POST: Friendship/Invite
         [HttpPost]
-        public ActionResult Invite([Bind(Include = "Email")] string email)
+        public async Task<ActionResult> Invite([Bind(Include = "Email")] string email)
         {
             try
             {
                 var address = new MailAddress(email);
-                // Do something with the valid email address
+                await SendInvitationTokenAsync(address);
+
+                return RedirectToAction("Info", new { email });
             }
             catch (FormatException ex)
             {
                 // The email address was invalid
                 return View();
             }
-            return RedirectToAction("Info", new { email });
+
         }
 
         public ActionResult Info(string email)
@@ -53,6 +50,28 @@ namespace MyLibrary.Controllers
             ViewBag.Title = "Wysłano zaproszenie";
             ViewBag.Message = "Na adres " + email + " zostało wysłane zaproszenie do Twojej bibilioteki.";
             return View("Info");
+
+        }
+
+        public ActionResult WelcomeFriendToLibrary(string token)
+        {
+            return View("Info");
+        }
+
+        private async Task SendInvitationTokenAsync(MailAddress email)
+        {
+            var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            var UserManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            var callbackUrl = Url.Action("WelcomeFriendToLibrary", "Friendship",
+                   new { token }, protocol: Request.Url.Scheme);
+            string emailText = "Użytkownik " + user.FirstName + " " + user.SecondName
+                + " wysłał Ci zaproszenie do swojej biblioteki w systemie MyLibrary. Kliknij <a href=\""
+                + callbackUrl + "\">tutaj</a>, by uzyskać dostęp.";
+            string subject = "[MyLibrary] Zaproszenie do biblioteki od użytkownika " + user.UserName;
+            await UserManager.SendEmailAsync(user.Id, subject, emailText);
 
         }
     }
