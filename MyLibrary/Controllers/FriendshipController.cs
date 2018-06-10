943d4ed8180fdbe2583e8ffeda1aa9e8b6a28fcf
Oliwia <oliwia.m.96@wp.pl>
2018-06-10 12:55:13 +0200

    Fix destination adress in invitation link; Better messages

--- MyLibrary/Controllers/FriendshipController.cs
+++ MyLibrary/Controllers/FriendshipController.cs
@@ -72,24 +72,36 @@ namespace MyLibrary.Controllers
                 db.SaveChanges();
 
                 var duplicates = db.Friendships.Count(f => (f.LibraryId == friendship.LibraryId) && f.ApplicationUserId == friendship.ApplicationUserId);
-                if(duplicates > 1)
+                if (duplicates > 1)
                 {
-                    var list = db.Friendships.Where(f => (f.LibraryId == friendship.LibraryId) && f.ApplicationUserId == friendship.ApplicationUserId).OrderByDescending(f => f.StartOfFriendship);
+                    /*var list = db.Friendships.Where(f => (f.LibraryId == friendship.LibraryId) && f.ApplicationUserId == friendship.ApplicationUserId).OrderByDescending(f => f.StartOfFriendship);
                     var toRemove = list.First();
-                    db.Friendships.Remove(toRemove);
+                    db.Friendships.Remove(toRemove);*/
+
+                    db.Friendships.Remove(friendship);
                     db.SaveChanges();
                     ViewBag.Title = "Duplikat zaproszenia";
                     ViewBag.Message = "Już byłeś członkiem tej biblioteki.";
 
                 }
+                else if (friendship.ApplicationUserId == friendship.Library.ApplicationUser.Id)
+                {
+                    db.Friendships.Remove(friendship);
+                    db.SaveChanges();
+                    ViewBag.Title = "Błąd zaproszenia";
+                    ViewBag.Message = "Nie można zaprosić samego siebie do własnej bibilioteki.";
+
+                }
                 else
                 {
                     ViewBag.Title = "Akceptacja zaproszenia";
-                    ViewBag.Message = "Nastąpiła akcpetacja zaproszenia do biblioteki od użytkownika " + friendship.Library.ApplicationUser.UserName;
+                    ViewBag.Message = "Nastąpiła akcpetacja zaproszenia do biblioteki użytkownika " + friendship.Library.ApplicationUser.UserName + ".";
                 }
-
-
-                
+            }
+            else
+            {
+                ViewBag.Title = "Błąd tokenu";
+                ViewBag.Message = "Nie odnaleziono zaproszenia o danym tokenie.";
             }
             return View("Info");
         }
@@ -102,7 +114,7 @@ namespace MyLibrary.Controllers
             var user = UserManager.FindById(User.Identity.GetUserId());
             string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
 
-           
+
 
             var callbackUrl = Url.Action("WelcomeFriendToLibrary", "Friendship",
                    new { token }, protocol: Request.Url.Scheme);
@@ -110,7 +122,28 @@ namespace MyLibrary.Controllers
                 + " wysłał Ci zaproszenie do swojej biblioteki w systemie MyLibrary. Kliknij <a href=\""
                 + callbackUrl + "\">tutaj</a>, by uzyskać dostęp.";
             string subject = "[MyLibrary] Zaproszenie do biblioteki od użytkownika " + user.UserName;
-            await UserManager.SendEmailAsync(user.Id, subject, emailText);
+
+
+            var message = new MailMessage();
+            message.To.Add(email);
+
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = emailText;
+
+            message.From = new MailAddress("pisak.96@gmail.com", "MyLibrary Admin Oliwia");
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new System.Net.NetworkCredential("pisak.96@gmail.com", Environment.GetEnvironmentVariable("tajna_zmienna_AP1"));
+            var siemka = Environment.GetEnvironmentVariable("tajna_zmienna_AP1");
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+
+            using (smtpClient)
+            {
+                await smtpClient.SendMailAsync(message);
+            }
 
             var friendship = db.Friendships.Create();
             friendship.Library = user.Library;
@@ -118,7 +151,6 @@ namespace MyLibrary.Controllers
             friendship.StartOfFriendship = DateTime.Now;
             db.Friendships.Add(friendship);
             db.SaveChanges();
-
 
         }
     }
