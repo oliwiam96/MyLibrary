afa6e7d5f75ac72bc8a940e820260a14a60626e8
Oliwia <oliwia.m.96@wp.pl>
2018-06-29 01:25:03 +0200

    Change descriptions in views

--- MyLibrary/Models/Book.cs
+++ MyLibrary/Models/Book.cs
@@ -10,15 +10,17 @@ namespace MyLibrary.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Title is required!")]
-        [StringLength(100, ErrorMessage = "Maximal length of the title of a book is 100 characters!")]
+        [Display(Name = "Tytuł")]
+        [Required(ErrorMessage = "Tytuł jest wymagany!")]
+        [StringLength(100, ErrorMessage = "Maksymalna liczba znaków dla tytułu to 100!")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Author is required!")]
-        [StringLength(100, ErrorMessage = "Maximal length of the author of a book is 100 characters!")]
+        [Display(Name = "Autor")]
+        [Required(ErrorMessage = "Autor jest wymagany!")]
+        [StringLength(100, ErrorMessage = "Maksymalna liczba znaków dla autora to 100!")]
         public string Author { get; set; }
 
-        [Display(Name = "Photo of the cover")]
+        [Display(Name = "Link do zdjęcia okładki")]
         public string UrlPhoto { get; set; }
 
         public virtual ICollection<BookInLibrary> BooksInLibrary { get; set; }
